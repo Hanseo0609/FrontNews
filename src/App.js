a7eb64bcd95cbf495581e0cd28c9d207049dd66e
김승현 <seunghyun@gimseunghyeon-ui-MacBookPro.local>
2024-09-11 20:36:53 +0900

    frontend king

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-
+import Navbar from "../src/components/Navbar"
 function App() {
   return (
     <div className="App">
