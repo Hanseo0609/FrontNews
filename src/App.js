11ca3e97478bc4554eddcb3022f6cb52a74a9047
kjea123 <kjsk7898@gmail.com>
2024-09-12 11:38:40 +0900

    header update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "../src/components/Navbar"
+import Header from "./components/Header";
 import Line from "./components/Line";
 function App() {
   return (
     <div className="App">    
       <Navbar />
       <Line />
+      <Header />
     </div>
   );
 }
