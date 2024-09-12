dae31e9c3004783910677a8fd773df822e684d19
kjea123 <kjsk7898@gmail.com>
2024-09-12 17:38:02 +0900

    aaasq

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from "./components/Header";
 import Line from "./components/Line";
 function App() {
   return (
-    <div className="App">    
+    <div className="App">   
+      <p>asdasfafqw</p> 
       <Navbar />
       <Line />
       <Header />
