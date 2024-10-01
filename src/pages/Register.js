34ec9f4df60a25bfa97316f07dbdaa7cfa96f482
Eojin Choi <djwls1064@gmail.com>
2024-10-01 22:01:17 +0900

    자잘한 수정

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -108,6 +108,7 @@ export default function Login() {
           user_nickname: userNickname,
           user_age: userAge,  // 숫자값
         });
+        alert("회원가입 성공!!")
         document.location.href = '/Login'
         console.log(response.data);  // response.data로 출력
       } catch (error) {
