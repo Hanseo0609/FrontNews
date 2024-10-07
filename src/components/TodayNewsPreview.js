390d8473fc59a68d3e60a69c4711702ef2f8c27d
kjea123 <kjsk7898@gmail.com>
2024-10-07 20:03:58 +0900

    asdadasd

--- src/components/TodayNewsPreview.js
+++ src/components/TodayNewsPreview.js
@@ -1,17 +1,37 @@
 import { Link } from 'react-router-dom';
 import * as styleD from '../styles/NewsPreview';
 import Popup from './PopNews';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import React from 'react';
+import axios from 'axios';
+
 
 export default function TodayNewsPreview() {
+  const serverURL = process.env.REACT_APP_SERVER_URL;
+
   const [popup, setPopup] = useState([false, false, false]);
+  const [news, setNews] = useState([]);
 
   const togglePopup = (index) => {
-    const newPopup = [...popup];
-    newPopup[index] = !newPopup[index];
+    const newPopup = [...popup].map((value, i) => (i === index ? !value : false));
     setPopup(newPopup);
   };
+
+  const todaysnewsPreviewLoad = async () => {
+    try {
+			const response = await axios.get(`${serverURL}/users/newsPreview`);
+      if (response.data["status"] === 201) {
+        console.log(response.data);
+        setNews(response.data["data"]);
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    todaysnewsPreviewLoad();
+  }, []);  
 
   return (
     <div>
@@ -24,12 +44,14 @@ export default function TodayNewsPreview() {
             return (
               <styleD.TodayNewsContainer style={{ marginTop: 50 + 200 * index + 'px' }} key={index + 2}>
                 <styleD.NewsImg />
+
                 <div style={{ margin: '0px 0px 60px 30px', position: 'relative' }}>
                   <p style={{ fontWeight: 'bold' }}>제목</p>
                   <p className='todayNewsContent'>내용</p>
                   <styleD.SeeMoreBtn onClick={() => togglePopup(index)}>자세히</styleD.SeeMoreBtn>
                   <Popup isOpen={popup[index]} onClose={() => togglePopup(index)} />
                 </div>
+                
               </styleD.TodayNewsContainer>
             )
           })
