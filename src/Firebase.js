814347a43803373969c5bc8202a3fc3855c17a55
Eojin Choi <djwls1064@gmail.com>
2024-11-24 16:00:11 +0900

    asd

---
+++ src/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFfo7bwhEqLTVNncA3HLZRMNlYTm8BGLE",
+  authDomain: "coin-news-cd1f1.firebaseapp.com",
+  projectId: "coin-news-cd1f1",
+  storageBucket: "coin-news-cd1f1.appspot.com", // 수정됨
+  messagingSenderId: "323357288696",
+  appId: "1:323357288696:web:ae2731f2aece847496f30d"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
